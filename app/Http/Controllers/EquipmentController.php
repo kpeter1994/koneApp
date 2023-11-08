8d1a28447926555776a8e0b8d5862aea842c64eb
kpeter1994 <smitpeter777@gmail.com>
2023-11-08 10:43:59 +0100

    feed

--- app/Http/Controllers/EquipmentController.php
+++ app/Http/Controllers/EquipmentController.php
@@ -13,6 +13,7 @@ class EquipmentController extends Controller
     public function index(Request $request)
     {
         $search = $request->query('search');
+        $searchTerms = explode(' ', $search);
 
         $now = Carbon::now();
 
@@ -21,13 +22,19 @@ class EquipmentController extends Controller
             ->where('end_status', '>', $now)
             ->get();
 
-        $equipments = Equipment::where('name', 'LIKE', "%{$search}%")
-            ->orWhere('address', 'LIKE', "%{$search}%")
-            ->orWhere('equipment', 'LIKE', "%{$search}%")
-            ->orWhere('emi', 'LIKE', "%{$search}%")
-            ->limit(10)
-            ->get();
-        //dd($equipments) ;
+        $equipmentsQuery = Equipment::query();
+
+        foreach ($searchTerms as $term) {
+            $equipmentsQuery->where(function ($query) use ($term) {
+                $query->where('name', 'LIKE', "%{$term}%")
+                    ->orWhere('address', 'LIKE', "%{$term}%")
+                    ->orWhere('equipment', 'LIKE', "%{$term}%")
+                    ->orWhere('inventory_number', 'LIKE', "%{$term}%")
+                    ->orWhere('emi', 'LIKE', "%{$term}%");
+            });
+        }
+
+        $equipments = $equipmentsQuery->limit(10)->get();
 
         return Inertia::render('Equipment/Index', compact('equipments', 'orders'));
     }
