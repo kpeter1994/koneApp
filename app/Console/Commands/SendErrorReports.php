4075cf9db38a531989924b15fed7942390352b34
kpeter1994 <smitpeter777@gmail.com>
2023-12-10 10:31:32 +0100

    Error limit 150

--- app/Console/Commands/SendErrorReports.php
+++ app/Console/Commands/SendErrorReports.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Mail\TestEmail;
 use Illuminate\Support\Facades\Mail;
+use Exception;
 
 class SendErrorReports extends Command
 {
@@ -27,11 +28,27 @@ class SendErrorReports extends Command
      */
     public function handle()
     {
+        $maxAttempts = 5;
+        $attempts = 0;
+        $success = false;
         $date = date('Y_m_d');
-        Mail::to(['Nikolett.Beniczky@KONE.com', 'krisztian.eszenszky@kone.com', 'csaba.hegedus@kone.com', 'csilla.perenyi@kone.com', 'richard.kovacshegedus@kone.com', 'csaba.szereto@kone.com'])
-            ->bcc(['feher.peter@pandant.hu', 'smitpeter777@gmail.com'])
-            ->send(new TestEmail(storage_path('app/errors_'.$date.'.xlsx')));
 
-        $this->info('Error report email sent successfully.');
+        while (!$success && $attempts < $maxAttempts) {
+            try {
+                Mail::to(['Nikolett.Beniczky@KONE.com', 'krisztian.eszenszky@kone.com', 'csaba.hegedus@kone.com', 'csilla.perenyi@kone.com', 'richard.kovacshegedus@kone.com', 'csaba.szereto@kone.com'])
+                    ->bcc(['feher.peter@pandant.hu', 'smitpeter777@gmail.com'])
+                    ->send(new TestEmail(storage_path('app/errors_'.$date.'.xlsx')));
+
+                $success = true;
+                $this->info('Error report email sent successfully.');
+            } catch (Exception $e) {
+                $attempts++;
+                sleep(5); // Várakozás a következő próbálkozás előtt
+            }
+        }
+
+        if (!$success) {
+            $this->error('Failed to send error report email after ' . $maxAttempts . ' attempts.');
+        }
     }
 }
